3e3b0a1f8a4117e6a5843665e15d104364741540
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-01-25 01:22:16 +0400

    feature/adding 3 javascript file
    Adding 3 javascript file

---
+++ script4.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let temperature = Number(prompt("Enter degree"));
+
+
+// Write a function called checkTemperature that takes a temperature value as an argument.
+// Use conditional statements to categorize the temperature as:
+// Freezing for below 0.
+// Cold for 0-20.
+// Warm for 21-30.
+// Hot for above 30.
+// Test the function with at least four different temperature values.
